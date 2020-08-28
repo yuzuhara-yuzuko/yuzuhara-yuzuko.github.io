51070831a6519df3c2d5415a32f7653626bc2bf8
Yuzuhara Yuzuko <yuzuhara.yuzuko@gmail.com>
2020-08-28 17:18:48 +0900

    links

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,14 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">
-        yuzu
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <ul>
+        <li><a href="https://twitter.com/makudona_poteko">@makudona_poteko</a></li>
+        <li><a href="https://note.com/yuzukomemo/n/n2edbf5a98d6c">ゆずもめも（noteホーム）</a></li>
+        <li><a href="https://note.com/yuzukomemo/n/n7020e7d594a0">ゆずこの録音環境</a></li>
+        <li><a href="https://note.com/yuzukomemo/n/n07d8ff90a220">ゆずこの音域</a></li>
+        <li><a href="https://twitter.com/search?q=from%3Amakudona_poteko%20%E3%82%86%E3%81%9A%E3%81%93%E3%83%A9%E3%83%B3%E3%83%81&src=typed_query&f=image">ゆずこランチ</a></li>
+        <li><a href="https://twitter.com/search?q=from%3Amakudona_poteko%20%E9%9F%B3%E6%A5%BD%E6%B4%BB%E5%8B%95&src=typed_query&f=image">音楽活動</a></li>
+      </ul>
     </div>
   </div>
 </template>
