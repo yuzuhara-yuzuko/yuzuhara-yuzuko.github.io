a6671b198610428e53b702ba6de3e4f27a300248
Yuzuhara Yuzuko <yuzuhara.yuzuko@gmail.com>
2020-08-28 17:51:25 +0900

    2020-08-28-1751

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div>
-      ゆずこ
+      ゆずこです
       <ul>
         <li><a href="https://twitter.com/makudona_poteko">@makudona_poteko</a></li>
         <li><a href="https://note.com/yuzukomemo/n/n2edbf5a98d6c">ゆずもめも（noteホーム）</a></li>
